56f6c57a51ea761de3dc3b05674202aad82a0927
Pravalika Chunarkar <99178842+Pro-pravalika@users.noreply.github.com>
2022-02-18 15:31:52 +0530

    Add files via upload

---
+++ Using operators .sql
@@ -0,0 +1,7 @@
+select * from test;
+select distinct name from test;
+select * from test where name = 'jacob';
+select * from test where marks='100';
+select * from test where marks > '50' and class = '10';
+select * from test where marks = '100' or marks = '70';
+select * from test where not marks > '40';
