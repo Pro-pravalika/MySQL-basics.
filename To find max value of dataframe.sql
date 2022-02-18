4d97435bce4b35b0cda2fc12e86a201d34e350c4
Pravalika Chunarkar <99178842+Pro-pravalika@users.noreply.github.com>
2022-02-18 16:09:02 +0530

    Add files via upload

---
+++ To find max value of dataframe.sql
@@ -0,0 +1,13 @@
+select * from emp1;
+insert into emp1 values(1,'korea',1000);
+insert into emp1 values(2,'lika',1200);
+insert into emp1 values(3,'prava',1300);
+insert into emp1 values(4,'pandu',1000);
+insert into emp1 values(5,'valli',1500);
+insert into emp1 values(6,'pranavi',1600);
+insert into emp1 values(7,'srilika',10000);
+insert into emp1 values(8,'hjgk',10900);
+
+
+
+select max(e_salary) from emp1;
